9a3f7d0cb821ed11118d7871004492dad1f23f4d
Kurnia D Win <kurnia.d.win@gmail.com>
2018-11-15 18:34:24 +0700

    range nil is working

--- util.go
+++ util.go
@@ -7,10 +7,8 @@ func MergeHeader(dst, src http.Header) http.Header {
 	if dst == nil {
 		dst = make(http.Header)
 	}
-	if src != nil {
-		for k, v := range src {
-			dst[k] = append(dst[k], v...)
-		}
+	for k, v := range src {
+		dst[k] = append(dst[k], v...)
 	}
 	return dst
 }
