16e8a64d0d0134c414a7a32b6c6348dd6f31e87e
Kurnia D Win <kurnia.d.win@gmail.com>
2020-03-09 23:27:08 +0700

    implement http.Handler for *Response

--- handler.go
+++ handler.go
@@ -2,16 +2,34 @@ package handler
 
 import "net/http"
 
+// Note to anyone who read this sourcecode
+// (*Response) and http.HandlerFunc are isomorphic, so it can be converted to each other
+// -- *Response        -> http.HandlerFunc : use (*Response).ServeHTTP
+// -- http.HandlerFunc -> *Response        : use New
+
 // Response representation.
 type Response struct {
 	extraHeader http.Header
 	handler     http.HandlerFunc
 }
 
+// ServeHTTP execute the resp
+func (resp *Response) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if resp == nil {
+		return
+	}
+
+	mergeHeader(w.Header(), resp.extraHeader)
+
+	if resp.handler != nil {
+		resp.handler(w, r)
+	}
+}
+
 // Of wrap h into normal http.HandlerFunc
 func Of(h func(*http.Request) *Response) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		Execute(h(r), w, r)
+		h(r).ServeHTTP(w, r)
 	}
 }
 
@@ -23,14 +41,8 @@ func New(handler http.HandlerFunc) *Response {
 }
 
 // Execute the resp
+//
+// DEPRECATED: use (*Response).ServeHTTP instead
 func Execute(resp *Response, w http.ResponseWriter, r *http.Request) {
-	if resp == nil {
-		return
-	}
-
-	mergeHeader(w.Header(), resp.extraHeader)
-
-	if resp.handler != nil {
-		resp.handler(w, r)
-	}
+	resp.ServeHTTP(w, r)
 }
