8024ba1deff0c91e35285a0ff2f3fe3e6784de81
Kurnia D Win <kurnia.d.win@gmail.com>
2020-06-03 11:00:28 +0700

    update

--- defresponse/json.go
+++ defresponse/json.go
@@ -17,3 +17,16 @@ func JSON(status int, data interface{}) handler.Response {
 		json.NewEncoder(w).Encode(data)
 	})
 }
+
+// JSONPretty as Response.
+// the error of json.Encoder.Encode is ignored.
+func JSONPretty(status int, data interface{}) handler.Response {
+	return handler.New(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(status)
+
+		enc := json.NewEncoder(w)
+		enc.SetIndent("", "  ")
+		enc.Encode(data)
+	})
+}
